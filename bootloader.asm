7fd7760da935ef66a279de6d0be684c21598b7d6
didi123 <hanifikorkmaz38@hotmail.com>
2025-10-25 20:05:13 +0300

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -8,22 +8,32 @@ start:
     int 0x10
     mov al, 'S'
     int 0x10
+    mov ax, 0x1000
+    mov es, ax
+    mov bx, 0
+    mov cx, 4
+.load_kernel:
+    cmp cx, 0
+    je run_kernel
 
     mov ah, 0x02
-    mov al, 0x01
-    mov ch, 0x00
-    mov cl, 0x02
-    mov dh, 0x00
-
-    mov al, [BOOT_DRIVE]
-    mov dl, al
-
-    mov bx, 0x0000
-    mov ax, 0x1000
+    mov al, 1
+    mov ch, 0
+    mov cl, bl
+    add cl, 2
+    mov dh, 0
+    mov dl, [BOOT_DRIVE]
     int 0x13
     jc disk_error
 
+    inc bx
+    dec cx
+    jmp .load_kernel
+
+run_kernel:
     mov ax, 0x1000
+    mov ds, ax
+    mov es, ax
     mov ss, ax
     mov sp, 0xFFFF
 
@@ -34,10 +44,10 @@ disk_error:
     mov al, 'E'
     int 0x10
 
+
+
 hang:
    jmp hang
-
- BOOT_DRIVE db 0
 
 times 510-($-$$) db 0
 dw 0xAA55
