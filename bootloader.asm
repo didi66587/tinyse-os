08a39cd7dab2eaf2a505deeaedd5edbd89d72ea4
didi123 <hanifikorkmaz38@hotmail.com>
2025-10-26 11:19:26 +0300

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,21 +1,30 @@
 org 0x7C00
 
+
 start:
     cli
     cld
-    mov bl, dl
-    xor si, si
-    xor bx, bx
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov [BOOT_DRIVE], dl
+
 
     mov ah, 0x0E
     mov al, 'O'
     int 0x10
     mov al, 'S'
     int 0x10
-    mov ax, 0x1000
+
+    xor si, si
+    mov bx, 0x1000
     mov es, ax
-    mov bx, 0
-    mov cx, 4
+
+
+xor bl, bl
 .load_kernel:
     cmp si, 4
     je run_kernel
@@ -23,13 +32,11 @@ start:
     mov ah, 0x02
     mov al, 1
     mov ch, 0
-
-    mov ax, si
-    mov cl, 2
-    add cl, al
-
     mov dh, 0
-    mov dl, bl
+    mov cl, 2
+    add cl, bl
+
+    mov dl, [BOOT_DRIVE]
     int 0x13
     jc disk_error
 
@@ -38,23 +45,21 @@ start:
     jmp .load_kernel
 
 run_kernel:
+    cli
     mov ax, 0x1000
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0xFFFF
-
     jmp 0x1000:0x0000
 
 disk_error:
     mov ah, 0x0E
     mov al, 'E'
     int 0x10
+    jmp $
 
-
-
-hang:
-   jmp hang
+BOOT_DRIVE: db 0
 
 times 510-($-$$) db 0
 dw 0xAA55
